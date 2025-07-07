24c613e4ac83170e26fd77507682200d939745f2
shorenko <serhii.horenko@cm.com>
2025-07-07 20:16:53 +0200

    Initial Commit

---
+++ BUUTSHorenko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BUUTSHorenko
+//
+//  Created by Serhii Horenko | CM.com on 07/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
